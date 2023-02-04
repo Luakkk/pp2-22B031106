94bce18d994bd1e072097466086dbb7c7c1daf9c
Aluakx <122995436+Aluakx@users.noreply.github.com>
2023-02-04 23:09:47 +0600

    Add files via upload

---
+++ tsis2/accesssets.py
@@ -0,0 +1,12 @@
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+
+
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
---
+++ tsis2/sets.py
@@ -0,0 +1,52 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+# Note: the set list is unordered, meaning: the items will appear in a random order.
+
+# Refresh this page to see the change in the result.
+
+
+
+
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+
+
+
+
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+
+
+
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
+
+
+
+
+set1 = {"abc", 34, True, 40, "male"}
+
+print(set1)
+
+
+
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+
+
+
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
